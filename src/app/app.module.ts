3c3023a0662c81f4df1377926a39df2c5c6a17dc
githubVijayakumar <mailvijayakumar.vinayagam@gmail.com>
2021-11-06 10:40:25 -0400

    this is first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {GoogleLoginProvider, SocialLoginModule} from 'angularx-social-login';
+import {AuthGuardService} from './auth-guard.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'mainpage', component: MainPageComponent, canActivate: [AuthGuardService]},
+      {path: '**', component: LoginComponent}
+    ]),
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    SocialLoginModule
+  ],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+         provider: new GoogleLoginProvider('608307221390-u65nnes4ahrd2hpaketq8d8ku75kiv9i.apps.googleusercontent.com')
+        }
+      ]
+    }
+  },
+    AuthGuardService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
